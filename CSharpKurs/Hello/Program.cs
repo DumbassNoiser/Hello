9d7de91bf584ff523783006c2b8f9b945c822553
Rafał Bromka <rafal.bromka@o2.pl>
2019-07-11 18:50:34 +0200

    pierwszekodowanie

--- CSharpKurs/Hello/Program.cs
+++ CSharpKurs/Hello/Program.cs
@@ -7,15 +7,36 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            string name = args[0];
+            for (; ; )
+            {
+                Console.Write("Wpisz swoje imie: ");
+                string name = Console.ReadLine();
 
-            Console.WriteLine("Witaj " + name);
+                Console.WriteLine("Witaj " + name);
 
+                Console.Write(" Wpisz ile masz lat: ");
 
-            ////foreach (var item in args)
-            ////{
-            ////    Console.WriteLine("Witaj " + item);
-            //}
+                int age;
+                bool result = int.TryParse(Console.ReadLine(), out age);
+
+                if (age > 18)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Brawo, jesteś pełnoletni możesz wypić browara");
+                }
+                else if (result == false)
+                {
+                    Console.WriteLine("Wprowadziłeś zły wiek");
+                }
+                else
+                {
+                    Console.WriteLine("Możemy Ci zaoferować mleko");
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+                Console.ResetColor();
+            }                   
         }
     }
 }
